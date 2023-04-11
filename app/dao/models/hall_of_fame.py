e93d1bc994d6b121b720066e23097e5f9f6cc695
Sergei Popov <sergei.v.popov@icloud.com>
2023-04-12 02:53:59 +0300

    ref: Меняет структуру таблицы записи результатов

--- app/dao/models/hall_of_fame.py
+++ app/dao/models/hall_of_fame.py
@@ -9,27 +9,27 @@ class HallOfFameModel(BaseModel):
     turns = Column(Integer)
 
     u1_name = Column(String(255))
-    u1_class_id = Column(Integer, ForeignKey('class_type.id'))
+    u1_class = Column(String(255))
     u1_weapon_id = Column(Integer, ForeignKey('weapon.id'))
     u1_armor_id = Column(Integer, ForeignKey('armor.id'))
 
     u2_name = Column(String(255))
-    u2_class_id = Column(Integer, ForeignKey('class_type.id'))
+    u2_class = Column(String(255))
     u2_weapon_id = Column(Integer, ForeignKey('weapon.id'))
     u2_armor_id = Column(Integer, ForeignKey('armor.id'))
 
-    u1_class = relationship('ClassTypeModel',
-                            foreign_keys=[u1_class_id],
-                            )
+    # u1_class = relationship('ClassTypeModel',
+    #                         foreign_keys=[u1_class_id],
+    #                         )
     u1_weapon = relationship('WeaponModel',
                              foreign_keys=[u1_weapon_id],
                              )
     u1_armor = relationship('ArmorModel',
                             foreign_keys=[u1_armor_id],
                             )
-    u2_class = relationship('ClassTypeModel',
-                            foreign_keys=[u2_class_id],
-                            )
+    # u2_class = relationship('ClassTypeModel',
+    #                         foreign_keys=[u2_class_id],
+    #                         )
     u2_weapon = relationship('WeaponModel',
                              foreign_keys=[u2_weapon_id],
                              )
