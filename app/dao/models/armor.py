ee61bdbd9f39517ffdacb5e8ebbd77af2831d654
Sergei Popov <sergei.v.popov@icloud.com>
2023-04-12 02:52:36 +0300

    fix: опечатка

--- app/dao/models/armor.py
+++ app/dao/models/armor.py
@@ -6,5 +6,5 @@ from .basemodel import BaseModel
 class ArmorModel(BaseModel):
     __tablename__ = 'armor'
     name = Column(String(255))
-    degence = Column(Float)
+    defence = Column(Float)
     stamina_per_turn = Column(Float)
